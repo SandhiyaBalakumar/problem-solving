ea14dcfe2235d77c2e855de297da519ce71e8f61
SandhiyaBalakumar <sandhiyasubramani34@gmail.com>
2024-07-14 14:29:29 +0530

    Add files via upload

--- Problem solving/countupperlower.py
+++ Problem solving/countupperlower.py
@@ -1,4 +1,4 @@
-
+"""
 R_file=open (r"example.txt")
 Cap_count=0
 Low_count=0
@@ -17,9 +17,9 @@ for x in R_file.read():
     if x.isdigit():
         Digit_count+=1   
     if x.isspace():
-        Space_count+=1  
+        Space_count+=1    
     else:
-        Special_count=Special_count+=1
+        Special_count=Special_count+1
 print("Upper Letters Found :",Cap_count)
 print("Lower Letters Found :",Low_count)
 print("Alphabetics Found :",Alpha_count)
@@ -27,6 +27,24 @@ print("Digits Found :",Digit_count)
 print("Spaces Found :",Space_count)
 print("Symbols Found :",Special_count)
 
+"""
 
+R_file=open (r"example.txt")
+upper, lower,alpha, special, digit =0, 0, 0, 0, 0
 
-
+for x in R_file.read():
+    if x.isupper():
+       upper+=1
+    if x.islower():
+        lower+=1
+    if x.isalpha():
+        alpha+=1   
+    if x.isdigit():
+        digit+=1      
+    else:
+        special+=1
+print("Upper Letters Found :",upper)
+print("Lower Letters Found :",lower)
+print("Alphabetics Found :",alpha)
+print("Digits Found :",digit)
+print("Symbols Found :",special)
